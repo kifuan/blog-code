117dc48b9e45bb58756fe85907daecdbdd186cec
kifuan <kifuan@foxmail.com>
2022-06-02 20:20:52 +0800

    update: js-design-patterns-memento

---
+++ js-design-patterns-memento/script.js
@@ -0,0 +1,44 @@
+class Staff {
+    constructor(name, salary) {
+        this.name = name
+        this.salary = salary
+    }
+
+    riseSalary(salary) {
+        this.salary += salary
+    }
+
+    store() {
+        const memento = JSON.stringify(this)
+        return memento
+    }
+
+    restore(memento) {
+        Object.assign(this, JSON.parse(memento))
+    }
+}
+
+class StaffCareTaker {
+    mementos = {}
+
+    set(key, memento) {
+        this.mementos[key] = memento
+    }
+
+    get(key) {
+        return this.mementos[key]
+    }
+}
+
+const careTaker = new StaffCareTaker()
+const staff = new Staff('Bob', 12500)
+
+careTaker.set(1, staff.store())
+
+staff.riseSalary(5000)
+console.log(staff.salary)
+// 17500
+
+staff.restore(careTaker.get(1))
+console.log(staff.salary)
+// 12500
