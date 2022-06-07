7a80bc91d3465a3d70bab5e114345b3b55c25e13
kifuan <kifuan@foxmail.com>
2022-06-07 10:36:27 +0800

    refactor: change len to depth

--- js-problem-operator-or/script.js
+++ js-problem-operator-or/script.js
@@ -1,11 +1,11 @@
 function createNode(val) {
-    return { val, len: 0, next: undefined }
+    return { val, depth: 0, next: undefined }
 }
 
-function len(node) {
-    // wrong:   node?.len || -1
-    // correct: node?.len ?? -1
-    return node ? node.len : -1
+function depth(node) {
+    // wrong:   node?.depth || -1
+    // correct: node?.depth ?? -1
+    return node ? node.depth : -1
 }
 
 function insert(node, val) {
@@ -14,13 +14,13 @@ function insert(node, val) {
     }
 
     node.next = insert(node.next, val)
-    node.len = len(node.next) + 1
+    node.depth = depth(node.next) + 1
 
     return node
 }
 
-let root = createNode(4)
-root = insert(root, 8)
-root = insert(root, 3)
+const root = [3, 7, 4].reduce((root, val) => {
+    return insert(root, val)
+}, undefined)
 
 console.log(root)
