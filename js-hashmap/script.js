daea90c0d04251e6730482f0f18cc4c0e7faf481
kifuan <kifuan@foxmail.com>
2022-06-13 11:05:29 +0800

    refactor: add test case for checking prime numbers

--- js-hashmap/script.js
+++ js-hashmap/script.js
@@ -23,6 +23,17 @@ function nextPrime(n) {
     return n
 }
 
+const primes = [
+    2, 3, 5, 7, 11, 13, 17, 19,
+    23, 29, 31, 37, 41, 43, 47,
+    53, 59, 61, 67, 71, 73, 79,
+    83, 89, 97,
+]
+
+for (let i = 1; i < 100; i++) {
+    console.assert(isPrime(i) === primes.includes(i), i)
+}
+
 class HashMap {
     elements = []
 
