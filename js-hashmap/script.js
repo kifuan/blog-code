a9740c1e23e703ac5614c93c2424bbc248f39d3a
kifuan <kifuan@foxmail.com>
2022-06-13 10:21:30 +0800

    refactor: use prime number as length

--- js-hashmap/script.js
+++ js-hashmap/script.js
@@ -1,3 +1,24 @@
+function isPrime(n) {
+    if (n <= 2) {
+        return false
+    }
+
+    const max = Math.floor(Math.sqrt(n))
+    for (let i = 2; i <= max; i++) {
+        if (n % i == 0) {
+            return false
+        }
+    }
+    return true
+}
+
+function nextPrime(n) {
+    while (!isPrime(n)) {
+        n++
+    }
+    return n
+}
+
 class HashMap {
     elements = []
 
@@ -6,6 +27,7 @@ class HashMap {
     }
 
     _createElements(length) {
+        length = nextPrime(length)
         this.elements = Array(length).fill({ empty: true, key: undefined, val: undefined })
     }
 
